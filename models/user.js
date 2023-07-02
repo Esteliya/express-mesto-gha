6b7843ed4e4679331aa48eed7a6bbb1ea1546066
Esteliya <avroradis@gmail.com>
2023-07-02 05:12:04 +0300

    fix: форматирование по тестам

--- models/user.js
+++ models/user.js
@@ -3,28 +3,28 @@ const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
-  name: {
-    type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minLength: [2, 'Минимальная длина поля "name" - 2'],
-    maxLength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  about: {
-    type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
-    minLength: [2, 'Минимальная длина поля "name" - 2'],
-    maxLength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  avatar: {
-    type: String,
-    required: [true, 'Поле "avatar" должно быть заполнено'],
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректный URL',
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minLength: [2, 'Минимальная длина поля "name" - 2'],
+      maxLength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    about: {
+      type: String,
+      required: [true, 'Поле "about" должно быть заполнено'],
+      minLength: [2, 'Минимальная длина поля "name" - 2'],
+      maxLength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'Поле "avatar" должно быть заполнено'],
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: 'Некорректный URL',
+      },
     },
   },
-},
-{ versionKey: false },
+  { versionKey: false },
 );
 const User = mongoose.model('user', userSchema);
 
